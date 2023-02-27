8879c283181287efb698f1cd40c1c172908ab1db
shubhampawar077 <126279319+shubhampawar077@users.noreply.github.com>
2023-02-28 00:35:06 +0530

    Add files via upload

---
+++ Task1/script.js
@@ -0,0 +1,33 @@
+let string = "";
+let buttons = document.querySelectorAll('.btn');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'c'){
+      string = ""
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'del'){
+      string = string.slice(0,(string.length-1))
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == '%'){
+      string = (string/100)
+      document.querySelector('input').value = string;
+    }
+
+
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
+})
+
+
+
+
